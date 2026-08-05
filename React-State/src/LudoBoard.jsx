361bd3c65f341651bf97a78e783e835100322e34
Nisar Ali Khan <fatimanisar422@gmail.com>
2026-08-05 21:44:13 +0500

    Array and State Add

--- React-State/src/LudoBoard.jsx
+++ React-State/src/LudoBoard.jsx
@@ -3,15 +3,19 @@ import { useState } from 'react';
 export default function LudoBoard() {
 
     let [moves, setMoves] = useState({blue: 0,  yellow: 0, green: 0,red: 0})
+    let [arr, Setarr] = useState(["no moves yet"])
 
     let updateMoves = (color) => {
         console.log(`${color.charAt(0).toUpperCase() + color.slice(1)} Moves: ${moves[color]}`);
         setMoves({...moves, [color]: moves[color] + 1} );
     }
 
+    Setarr([...arr, `Blue Moves = ${moves.blue}`, `Yellow Moves = ${moves.yellow}`, `Green Moves = ${moves.green}`, `Red Moves = ${moves.red}`])
+
 return (
     <div>
         <p>Game Begins! </p> 
+        <p>{arr}</p>
         <div className="board">
             <p>Blue Moves = {moves.blue} </p>
             <button style={{backgroundColor: 'lightblue'}} onClick={() => updateMoves('blue')}>+1</button>
