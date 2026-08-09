e7b8d10916f79a15a6e59c25a8a5307e16366bbe
Nisar Ali Khan <fatimanisar422@gmail.com>
2026-08-09 17:16:00 +0500

    Fix Props

--- React-State/src/Lottery.jsx
+++ React-State/src/Lottery.jsx
@@ -2,18 +2,23 @@ import { useState } from 'react'
 import './Lottery.css'
 import { genTicket } from './helper'
 
-export default function Lottery(n, winningSum) {
-    let [ticket, setTicket] = useState(genTicket(3))
-    let isWinning = sum(ticket) === winningSum
-    let buyTicket = () => {
-        setTicket(genTicket(n))
-    }
+export default function Lottery({ n = 3, winningSum = 15 } = {}) {
+    const [ticket, setTicket] = useState(genTicket(n))
+
+    const ticketSum = ticket.reduce((s, v) => s + v, 0)
+    const isWinning = ticketSum === winningSum
+
+    const buyTicket = () => setTicket(genTicket(n))
 
     return (
-        <div >
+        <div className="lottery-wrapper">
             <h1>Lottery Game!</h1>
-            <Ticket ticket={ticket} />
-            <button onClick={buyTicket}>New Ticket</button>
+            <div className="ticket">
+                {ticket.map((num, i) => (
+                    <span key={i}>{num}</span>
+                ))}
+            </div>
+            <button className="lottery-button" onClick={buyTicket}>New Ticket</button>
             <h3>{isWinning ? 'Congratulations! You won!' : 'Keep trying!'}</h3>
         </div>
     )
